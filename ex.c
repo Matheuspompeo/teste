85f2499c4e1403db18a09676cd4300aaf97d9925
MATHEUS DE CASTRO POMPEO <15104136@l1801349.portoalegre.pucrsnet.br>
2016-05-11 11:11:46 -0300

    Exeplo de Vetor

---
+++ ex.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include<stdlib.h>
+#define SIZE 10
+
+int main()
+{
+	int s[SIZE];
+	int j;
+	int total=0;
+	
+	for(j=0; j<SIZE; j++)
+	{
+		s[j]=2+2*j;
+	}
+	
+	for(j=0; j<SIZE; j++)
+	{
+		total+=s[j];
+	}
+	
+	printf("%s%13s\n","Elemento","Valor");	
+
+	for(j=0; j<SIZE; j++)
+	{
+		printf("%7d %13d\n", j,s[j]);
+	}
+
+	printf("%8s%13d\n","Total",total);
+
+return EXIT_SUCCESS;
+}
